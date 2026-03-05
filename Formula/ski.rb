110034aca1a3aeb5ace6cb9bee21490b0f5971a1
linhey <is.linhey@outlook.com>
2026-03-05 12:03:59 +0800

    chore(formula): update ski formula

--- Formula/ski.rb
+++ Formula/ski.rb
@@ -6,20 +6,17 @@ class Ski < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/linhay/SKIntelligence/archive/refs/tags/2.0.0.tar.gz"
-      sha256 "16a1feb96a148198d9775cd808dbeddbc5806bb3575fd5492c1035c465b8cbf0"
+      url "https://github.com/linhay/SKIntelligence/releases/download/2.0.0/ski-macos-arm64.tar.gz"
+      sha256 "f823707b838b3c761fb90cb197a5411b78c0d33ab4fe62ba97207a5fb9521e68"
     end
     on_intel do
-      url "https://github.com/linhay/SKIntelligence/archive/refs/tags/2.0.0.tar.gz"
-      sha256 "16a1feb96a148198d9775cd808dbeddbc5806bb3575fd5492c1035c465b8cbf0"
+      url "https://github.com/linhay/SKIntelligence/releases/download/2.0.0/ski-macos-x86_64.tar.gz"
+      sha256 "e22f5c6c6dce0faf6112c71f5ca6a5dc9528241c16af529ee557012b155798b7"
     end
   end
 
-  depends_on "swift" => :build
-
   def install
-    system "swift", "build", "-c", "release", "--product", "ski"
-    bin.install ".build/release/ski"
+    bin.install "ski"
   end
 
   test do
